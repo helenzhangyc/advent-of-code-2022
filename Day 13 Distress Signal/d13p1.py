6cc009bbcbb307e5a6e9e119f6cb4c00e3f3b37f
Yicheng Zhang <zychelenzhang@gmail.com>
2023-11-27 15:28:29 -0800

    d13p1

---
+++ Day 13 Distress Signal/d13p1.py
@@ -0,0 +1,43 @@
+# It's way easier to do this in python so I did this day in python lol
+import ast 
+
+def compareNumber(l1, l2):
+    if l1 < l2:
+        return -1
+    elif l1 == l2:
+        return 0
+    else:
+        return 1
+
+def isRightOrder(l1, l2):
+    # both values are integers
+    if isinstance(l1, int) and isinstance(l2, int):
+        return compareNumber(l1, l2)
+    elif isinstance(l1, int) and isinstance(l2, list):
+        l1 = [l1]
+    elif isinstance(l2, int) and isinstance(l1, list):
+        l2 = [l2]
+
+    for a, b in zip(l1, l2):
+        result = isRightOrder(a, b)
+        if result != 0:
+            return result
+    return compareNumber(len(l1), len(l2))
+    
+    
+
+with open('input.txt') as f:
+    lines = f.readlines()
+    length = len(lines)
+    sum = 0
+    index = 1
+    for i in range(0, length, 3):
+        l1 = ast.literal_eval(lines[i])
+        l2 = ast.literal_eval(lines[i + 1])
+        # compare l1 and l2
+        if isRightOrder(l1, l2) == -1:
+            sum += index
+        index += 1
+    print(sum)
+
+    f.close()
